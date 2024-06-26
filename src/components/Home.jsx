d11ba40b697cb1e7fa7f0bd8a42dca824d3bc497
sofijalazarova <94320367+sofijalazarova@users.noreply.github.com>
2024-06-26 18:39:30 +0200

    Portfolio updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,12 +6,12 @@ const Home = () => {
   return (
     <section className={`relative w-full h-screen mx-auto justify-items-center pt-20`} style={{backgroundColor: "#f5f5f5"}}>
 
-      <div className='flex items-center px-12 py-12 gap-10'>
+      <div className='flex flex-col lg:flex-row items-center px-10 lg:px-12 py-12 gap-10'>
         <div className={`flex justify-center items-center w-1/2`}>
           <img className='object-fill' src={Image}/>
         </div>
 
-        <div className='w-1/2 text-left text-black pr-8'>
+        <div className='lg:w-1/2 text-center lg:text-left text-black lg:pr-8'>
           <h1 className={styles.heroSubText}>Aspiring Software Engineer</h1>
           <p className='mt-3'>I'm Sofija Lazarova, a dedicated and enthusiastic software engineering student at the Faculty of Computer Science & Engineering in Skopje. With all my exams completed, I am on the brink of graduating, ready to step into the professional world of software development.</p>
         </div>        
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -5,25 +5,17 @@ import { fadeIn, textVariant } from "../utils/motion";
 import Image from '../../public/galaxia1.png';
 import Image1 from '../../public/cinemaApp.png';
 import GitImage from '../../public/github.png';
+import Image2 from '../../public/makeupapp.png';
 
 const ProjectCard = ( props ) => {
   return (
-    <motion.div variants={fadeIn("up", "spring", 0.75)}>
-      <div
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className='p-5 rounded-xl sm:w-[360px] w-full' style={{backgroundColor: "#d1e2f3"}}
-      >
+    <div>
+      <div className='p-5 rounded-xl lg:w-[360px] w-full' style={{backgroundColor: "#d1e2f3"}}>
         <div className='relative w-full h-[230px]'>
-          <img
+          <img onClick={() => { window.location.href(`${props.website_link}`)}}
             src={props.imagePath}
             alt='project_image'
-            className='w-full h-full object-cover rounded-2xl'
-          />
-
+            className='w-full h-full object-cover rounded-2xl'/>
           <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
             <div
               onClick={() => window.open(source_code_link, "_blank")}  // link do github
@@ -41,34 +33,33 @@ const ProjectCard = ( props ) => {
         </div>
 
         <div className='mt-5'>
-          <h3 className='text-black font-bold text-[24px]'>{props.name}</h3>
+          <a href={props.website_link}><h3 className='text-black font-bold text-[24px]'>{props.name}</h3></a>
           <p className='mt-2 text-black text-[14px]'>{props.description}</p>
+          <p className="text-black font-bold">{props.tech}</p>
         </div>        
       </div>
-    </motion.div>
+    </div>
   );
 };
 
 const Works = () => {
   return (
     <>
-    <div className="mx-28 pb-20 pt-28" >
-      <motion.div variants={textVariant()}>
+    <div className="mx-28 pb-20 pt-28 text-center lg:text-left">
+      <div variants={textVariant()}>
         <p className={`${styles.sectionSubText}`} style={{fontSize: "30px"}}>My work</p>
-      </motion.div>
+      </div>
 
       <div className='w-full flex'>
-        <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
-          className='mt-3 text-[17px] max-w-3xl leading-[30px] text-black'
-        >
+        <p className='mt-3 text-[17px] max-w-3xl leading-[30px] text-black'>
           The following projects demonstrate my skills and experience through practical examples.
-        </motion.p>
+        </p>
       </div>
 
       <div className='mt-10 flex flex-wrap gap-7'>      
-          <ProjectCard imagePath={Image} name="Galaxia courses" description="This web application enables the sale of movie tickets, utilizing Onion Architecture to maintain a clean and structured codebase." github="https://github.com/sofijalazarova/galaxia/tree/main/galaxia"/>
-          <ProjectCard imagePath={Image1} name="Cinema App" description="This web application enables the sale of movie tickets, utilizing Onion Architecture to maintain a clean and structured codebase." github="https://github.com/sofijalazarova/EShopCinemaApp"/>
+          <ProjectCard imagePath={Image} name="Galaxia courses" description="This project aims to provide an introductory platform for users interested in learning about astronomy." github="https://github.com/sofijalazarova/galaxia/tree/main/galaxia" website_link="" tech="Spring Boot, React, Vite, Tailwind CSS, PostgreSQL"/>
+          <ProjectCard imagePath={Image1} name="Cinema App" description="This web application enables the sale of movie tickets, utilizing Onion Architecture to maintain a clean and structured codebase." github="https://github.com/sofijalazarova/EShopCinemaApp" website_link="https://cinego-sofija.azurewebsites.net/" tech="C# HTML CSS JavaScript, .NET Core"/>
+          <ProjectCard imagePath={Image2} name="Cinema App" description="In this project, I developed a dynamic and responsive web application that allows users to browse and discover various makeup products from Maybelline. Public API Used: http://makeup-api.herokuapp.com/" github="https://github.com/sofijalazarova/makeUpApp" website_link="https://sofijalazarova.github.io/makeUpApp/" tech="React, Vite, Tailwind CSS, Axios"/>
       </div>
       </div>
     </>
