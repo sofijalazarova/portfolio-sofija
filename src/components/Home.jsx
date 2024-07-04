ed6dfd5a06e33ae957e55c5ae3c597fa0e5064a9
sofijalazarova <94320367+sofijalazarova@users.noreply.github.com>
2024-07-04 16:04:48 +0200

    Change of project name

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
 
         <div className='lg:w-1/2 text-center lg:text-left text-black lg:pr-8'>
           <h1 className={styles.heroSubText}>Aspiring Software Engineer</h1>
-          <p className='mt-3'>I'm Sofija Lazarova, a dedicated and enthusiastic software engineering student at the Faculty of Computer Science & Engineering in Skopje. With all my exams completed, I am on the brink of graduating, ready to step into the professional world of software development.</p>
+          <p className='mt-3'>I'm <span className={styles.heroSubText}><b>Sofija Lazarova</b></span>, a dedicated and enthusiastic software engineering student at the Faculty of Computer Science & Engineering in Skopje. With all my exams completed, I am on the brink of graduating, ready to step into the professional world of software development.</p>
         </div>        
       </div>
       
